09f24e156569e5757b2022e171ee4b10f23dcc68
cegallo2 <cegallo2@gmail.com>
2017-02-14 11:40:28 -0600

    Add files via upload

---
+++ strip.cpp
@@ -0,0 +1,36 @@
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main () {
+
+    int i;
+
+    string   inputfilename;
+    ifstream inputfile;
+    string   residueinputname;
+    string   atom;
+    string     atomlist;
+    
+    // Prompt user for input file + residue
+    cout << "Enter input filename: " ;
+    cin  >> inputfilename;
+    cout << "Enter name of residue to be removed:";
+    cin  >> residueinputname;
+
+    // Sanity check on the file stream
+    inputfile.open(inputfilename.c_str());
+    if ( !inputfile.is_open() ) {
+        cout << "Cannot open input file: " 
+             << inputfilename
+             << endl;
+        return 1;
+    }
+}
+
+
+

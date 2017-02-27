73ef4694a128876d70158f71b3633363fc86a62c
Caleb Edward Gallops <cegallo2@gmail.com>
2017-02-27 11:33:40 -0600

    fixed doxconfig

--- strip.cpp
+++ strip.cpp
@@ -1,9 +1,30 @@
+/**
+ * @mainpage The Car Database Program
+ *
+ * Welcome to the Chemistry_Strip documentation. Here you will find all the relevant info
+ * related to this program which was designed to remove residues from AMBER suite 
+ * trajectory and topology files.
+ */
 
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <list>
 #include <cmath>
+
+/**
+ * @short   Chemistry_Strip
+ * @file    strip.cpp
+ * @author  Caleb Gallops & Dennis Kennetz
+ * @param   none
+ * @return  0 on success
+ *
+ * This program removes any specified residue from AMBER suite trajectory and topology files 
+ * It also simultaneously produces radial distribution functions for the removed residue.
+ * This is intended to reduce the size of these files so quicker analyses can be performed.  
+ * The purpose of producing an radial distribution is to extract any valuable information 
+ * before the strip. 
+ */
 
 using namespace std;
 

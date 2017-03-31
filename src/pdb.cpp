5bc7d3a696c24401d7952779429661d4bee8560f
Caleb Edward Gallops <cegallo2@gmail.com>
2017-03-31 13:39:57 -0500

    pdb.cpp can read in pdb files with TERs

--- src/pdb.cpp
+++ src/pdb.cpp
@@ -11,21 +11,36 @@ using namespace std;
 
 #include "pdb.h"
 
-bool Pdb::read_pdb ( string pdbfilename, Atom db[1000], int & size ) {
+bool Pdb::read_pdb ( string pdbfilename, Atom db[110], int & size ) {
 
     ifstream ifile( pdbfilename );
+    string ter = "TER";
+    string blank = "";
     if ( !ifile.is_open() ) return false;
+
     size = 0;
 
-    while ( size < 1000 ) {
-
-        if ( !(ifile >> db[size].atom_type >> db[size].atom_number >> db[size].element >> db[size].type >> db[size].residue_number >> db[size].x_coord >> db[size].y_coord >> db[size].z_coord >> db[size].ligand_distance >> db[size].score ) )
-            break;
-        size++;
-        }
+    for ( int i = 0; i < 110; i = i + 1 ) {
+      
+      ifile >> db[size].atom_type ;
+        if ( db[size].atom_type == ter ) {
+	    continue;
+        } 
+        ifile        >> db[size].atom_number 
+                     >> db[size].element 
+                     >> db[size].type 
+                     >> db[size].residue_number 
+                     >> db[size].x_coord 
+                     >> db[size].y_coord 
+                     >> db[size].z_coord 
+                     >> db[size].ligand_distance 
+	             >> db[size].score;
+	     size++;  
+        
+    }
     return true;
 }
 
-Pdb::Atom pdb[1000];
+Pdb::Atom pdb[110];
 int Pdb::number_of_atoms;
 string Pdb::pdbfilename;

e600cb95adffa226f9a157e10845897ed2423c75
drknnetz <drknnetz@memphis.edu>
2017-04-06 10:54:16 -0500

    remove_pbc.h
    header file for remove_pbc.cpp

---
+++ src/remove_pbc.h
@@ -0,0 +1,19 @@
+//remove_pbc.h
+#ifndef _REMOVE_PBC_H
+#define _REMOVE_PBC_H
+#endif
+#include <iostream>
+#include <fstream>     //file stream
+#include <ctime>       //system time
+#include <cstdlib>     //std lib c
+#include <math.h>      //math header
+#include <sstream>     //string stream
+#include <stdlib.h>    //standard library
+#include <time.h>      //time information header
+#include <sys/types.h> //system types header
+#include <fcntl.h>     //file open header
+#include <stdio.h>     //standard input output
+#include <string>      //string library
+#include <vector>      //vector library
+using namespace std;
+
